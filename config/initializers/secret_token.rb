9e7649b105dc8e5b4b68ac9964962049422e0947
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-04-22 19:31:20 +0900

    r new comugi -T --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comugi::Application.config.secret_token = '35f8de938b71a821676e5d741ad67bceefb0b5229dc8294d9bbdf136e556c1e4b26f6636e09904aebde9ce8191891e008cd3142b9ce959d58372431436638082'
