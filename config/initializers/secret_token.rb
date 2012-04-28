ab8a44f64d0260ad01844600596578d1d93fc2be
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-04-28 22:00:02 +0900

    SettingsLogic & OmniAuth

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Comugi::Application.config.secret_token = '35f8de938b71a821676e5d741ad67bceefb0b5229dc8294d9bbdf136e556c1e4b26f6636e09904aebde9ce8191891e008cd3142b9ce959d58372431436638082'
+Comugi::Application.config.secret_token = Settings.secret_token
