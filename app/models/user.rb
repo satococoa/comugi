93bb38de1318c710d197babb9d39844d1486cdce
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-04-28 22:38:59 +0900

    implement User.find_or_create_by_auth_hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   attr_accessible :image, :name, :nickname, :token, :uid
+
+  validates :uid, presence: true
+  validates :token, presence: true
+
+  def self.find_or_create_by_auth_hash(auth)
+    user = User.find_or_initialize_by_uid(auth['uid'])
+    user.name = auth['info']['name']
+    user.nickname = auth['info']['nickname']
+    user.image = auth['info']['image']
+    user.token = auth['credentials']['token']
+    user.save
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,49 @@
+# coding: utf-8
 require 'spec_helper'
 
 describe User do
-  describe "#create" do
-    subject { FactoryGirl.create :user }
-    it { should be_persisted }
+  describe "#valid?" do
+    context "必要な値が与えられているとき" do
+      subject { FactoryGirl.build :user }
+      it { should be_valid }
+    end
+    context "tokenがないとき" do
+      subject { FactoryGirl.build :user, token: nil }
+      it { should_not be_valid }
+    end
+    context "uidがないとき" do
+      subject { FactoryGirl.build :user, uid: nil }
+      it { should_not be_valid }
+    end
+  end
+  describe ".find_or_create_by_auth_hash" do
+    let(:auth_hash) {
+      {
+        'uid' => 'user_uid',
+        'info' => {
+          'name' => 'user_name',
+          'nickname' => 'user_nickname',
+          'image' => 'http://example.com/sample.jpg'
+        },
+        'credentials' => {
+          'token' => 'access_token'
+        }
+      }
+    }
+    describe "User.count" do
+      subject { User.count }
+      context "初めてログインした場合" do
+        before do
+          User.find_or_create_by_auth_hash(auth_hash)
+        end
+        it { should == 1 }
+      end
+      context "ログインは初めてではない場合" do
+        before do
+          2.times { User.find_or_create_by_auth_hash(auth_hash) }
+        end
+        it { should == 1 }
+      end
+    end
   end
 end
